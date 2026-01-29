d48d6bc1224a9916bd2c1387ece69dc960a078ad
term <medomoataz2hof@gmail.com>
2026-01-29 22:12:16 +0200

    .

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -2,13 +2,16 @@
 
 import { AuthProvider } from "@/context/AuthContext";
 import { RegistrationProvider } from "@/context/RegistrationContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function Providers({ children }) {
   return (
-    <AuthProvider>
-      <RegistrationProvider>
-        {children}
-      </RegistrationProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <RegistrationProvider>
+          {children}
+        </RegistrationProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
